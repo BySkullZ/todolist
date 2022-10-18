1d41b86c8da89792e6d583507f586d202d0f229c
BySkullZ <bahrimomo061@gmail.com>
2022-10-18 11:49:40 +0200

    design a faire pour de vrai

--- js/todolist.js
+++ js/todolist.js
@@ -1,6 +1,7 @@
 var category = "todo"
 
-let field = document.getElementById("add-field");
+let title = document.getElementById("add-field-title");
+let text = document.getElementById("add-field-text");
 let button = document.getElementById("add-button");
 
 function changeCategory(arg) {
@@ -13,13 +14,11 @@ function deleteCard(el) {
 }
 
 button.addEventListener("click", ()=> {
-    let text = field.value;
-    console.log(text);
-
     let card = document.getElementById("card")
     let cardClone = card.cloneNode(true)
     cardClone.style.display = "block";
-    cardClone.querySelector("#card-title").textContent = field.value;
+    cardClone.querySelector("#card-title").textContent = title.value;
+    cardClone.querySelector("#card-text").textContent = text.value;
     gridItems = document.getElementById(category)
     gridItems.appendChild(cardClone)
 })
