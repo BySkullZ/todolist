f50fc4d5072ea989ccf88b5e6a8afe54569398ad
BySkullZ <bahrimomo061@gmail.com>
2022-10-18 13:07:03 +0200

    aa

--- js/todolist.js
+++ js/todolist.js
@@ -1,24 +1,43 @@
-var category = "todo"
+var category = "todo";
 
 let title = document.getElementById("add-field-title");
 let text = document.getElementById("add-field-text");
-let button = document.getElementById("add-button");
+let addButton = document.getElementById("add-button");
+let categoryButton = document.getElementById("todolist-dropdown");
+
+var categoriesName = new Map();
+categoriesName.set("todo", "A faire");
+categoriesName.set("inprogress", "En cours");
+categoriesName.set("finished", "Terminé");
 
 function changeCategory(arg) {
-    category = arg
-    console.log(category)
+    category = arg;
+    categoryButton.querySelector("button").textContent = "Catégorie: " + categoriesName.get(category) + " ";
+}
+
+function editCard(el) {
+    console.log(el.parentElement.q)
+    el.parentElement.querySelector("editsection").style.display = "block";
 }
 
 function deleteCard(el) {
+    const listParent = el.parentNode.parentNode.parentNode.id
     el.parentNode.parentNode.remove();
+    updateChildElementCount(listParent);
 }
 
-button.addEventListener("click", ()=> {
-    let card = document.getElementById("card")
-    let cardClone = card.cloneNode(true)
+function updateChildElementCount(cat) {
+    let gridItems = document.getElementById(cat);
+    gridItems.querySelector("h2").textContent = categoriesName.get(cat) + " (" + (gridItems.childElementCount - 1) + ")";
+}
+
+addButton.addEventListener("click", ()=> {
+    let card = document.getElementById("card");
+    let cardClone = card.cloneNode(true);
     cardClone.style.display = "block";
     cardClone.querySelector("#card-title").textContent = title.value;
     cardClone.querySelector("#card-text").textContent = text.value;
-    gridItems = document.getElementById(category)
-    gridItems.appendChild(cardClone)
+    let gridItems = document.getElementById(category);
+    gridItems.appendChild(cardClone);
+    gridItems.querySelector("h2").textContent = categoriesName.get(category) + " (" + (gridItems.childElementCount - 1) + ")";
 })
